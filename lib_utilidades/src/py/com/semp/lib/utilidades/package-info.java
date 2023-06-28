8baa0151586f3509681c4cb94e029d7152a6331f
Sergio Morel <sergio.morel@pti.org.py>
2023-06-28 19:10:30 -0400

    Modificacion de información de los paquetes.

--- lib_utilidades/src/py/com/semp/lib/utilidades/package-info.java
+++ lib_utilidades/src/py/com/semp/lib/utilidades/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Paquete de la Librer&iacute;a de utilidades
- * @author sergiomorel
+ * Paquete de la Librer&iacute;a de utilidades.
+ * 
+ * @author Sergio Morel
  */
 package py.com.semp.lib.utilidades;
--- lib_utilidades/src/py/com/semp/lib/utilidades/utilities/package-info.java
+++ lib_utilidades/src/py/com/semp/lib/utilidades/utilities/package-info.java
@@ -1,1 +1,6 @@
+/**
+ * Paquete para clases de utilidades varias.
+ * 
+ * @author Sergio Morel
+ */
 package py.com.semp.lib.utilidades.utilities;
