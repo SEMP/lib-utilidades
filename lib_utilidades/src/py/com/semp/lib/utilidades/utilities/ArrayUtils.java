b12b5faca0865ad54aff08f20fc2689be8e7d440
Sergio Morel <sergio.morel@pti.org.py>
2023-06-30 15:41:14 -0400

    Creation of the Utilities class and implementation of the equals method. Implementation of toHexaArrayString and contains. Correction of various methods.

--- lib_utilidades/src/py/com/semp/lib/utilidades/utilities/ArrayUtils.java
+++ lib_utilidades/src/py/com/semp/lib/utilidades/utilities/ArrayUtils.java
@@ -1,12 +1,15 @@
 package py.com.semp.lib.utilidades.utilities;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Class with static method with utilities for arrays.
+ * 
+ * @author Sergio Morel
+ */
 public final class ArrayUtils
 {
 	private ArrayUtils()
@@ -15,19 +18,29 @@ public final class ArrayUtils
 	}
 	
 	/**
-	 * Crea un arreglo nuevo con el contenido de los arreglos pasados por parametro.
-	 * Los valores son insertados en el nuevo arreglo union de acuerdo al orden de los
-	 * arreglos del parametro <b>arrays</b>.
-	 * @param arrays - conjunto de arrays
+	 * Concatenates the content of all the arrays received as parameter.
+	 * 
+	 * @param arrays
+	 * - arrays to concatenate.
 	 * @return
-	 * - nuevo arreglo correspondiente a la union de los arreglos de <b>arrays</b>.
+	 * - a new array with elements concatenated from the parameter arrays.
 	 */
 	public static byte[] join(byte[]... arrays)
 	{
+		if(arrays == null)
+		{
+			return null;
+		}
+		
 		int unionLength = 0;
 		
 		for(byte[] array : arrays)
 		{
+			if(array == null)
+			{
+				continue;
+			}
+			
 			unionLength += array.length;
 		}
 		
@@ -37,6 +50,11 @@ public final class ArrayUtils
 		
 		for(byte[] array : arrays)
 		{
+			if(array == null)
+			{
+				continue;
+			}
+			
 			for(int i = 0; i < array.length; i++)
 			{
 				union[unionIndex++] = array[i];
@@ -47,30 +65,49 @@ public final class ArrayUtils
 	}
 	
 	/**
-	 * Crea un arreglo nuevo con el contenido de los arreglos pasados por parametro.
-	 * Los valores son insertados en el nuevo arreglo union de acuerdo al orden de los
-	 * arreglos del parametro <b>arrays</b>.
-	 * @param arrays - conjunto de arrays
+	 * Concatenates the content of all the arrays received as parameter.
+	 * 
+	 * @param <T>
+	 * - type of the array's element.
+	 * @param arrays
+	 * - arrays to concatenate.
 	 * @return
-	 * - nuevo arreglo correspondiente a la union de los arreglos de <b>arrays</b>.
+	 * - a new array with elements concatenated from the parameter arrays.
 	 */
 	@SafeVarargs
-	public static <E> E[] join(E[]... arrays)
+	public static <T> T[] join(T[]... arrays)
 	{
+		if(arrays == null)
+		{
+			return null;
+		}
+		
 		int unionLength = 0;
 		
-		for(E[] array : arrays)
+		for(T[] array : arrays)
 		{
+			if(array == null)
+			{
+				continue;
+			}
+			
 			unionLength += array.length;
 		}
 		
+		Class<?> componentType = arrays.getClass().getComponentType().getComponentType();
+		
 		@SuppressWarnings("unchecked")
-		E[] union = (E[])Array.newInstance(arrays.getClass().getComponentType().getComponentType(), unionLength);
+		T[] union = (T[])Array.newInstance(componentType, unionLength);
 		
 		int unionIndex = 0;
 		
-		for(E[] array : arrays)
+		for(T[] array : arrays)
 		{
+			if(array == null)
+			{
+				continue;
+			}
+			
 			for(int i = 0; i < array.length; i++)
 			{
 				union[unionIndex++] = array[i];
@@ -81,39 +118,126 @@ public final class ArrayUtils
 	}
 	
 	/**
-	 * Obtiene un array con los elementos en comun de los 2 arrays
-	 * pasados como par&aacute;metro.
-	 * @param array1
-	 * primer array.
-	 * @param array2
-	 * segundo array.
+	 * Returns an array with only the elements every array has in common.
+	 * 
+	 * @param <T>
+	 * - type of the array's element.
+	 * @param arrays - arrays to check if they have elements in common.
 	 * @return
-	 * array con elementos en comun de los arrays pasados como par&aacute;metro.
+	 * - array with the elements in common.<br>
+	 * - an empty array if any of the input arrays is null.<br>
+	 * - <b>null</b> if the parameter received is null.
+	 * @author Sergio Morel
 	 */
-	public static <E> E[] intersect(E[] array1, E[] array2)
+	@SafeVarargs
+	public static <T> T[] intersect(T[]... arrays)
 	{
-		List<E> result = new LinkedList<>();
-		Set<E> hashSet = new HashSet<>();
-		
-		hashSet.addAll(Arrays.asList(array2));
-		
-		for(E element : array1)
+		if(arrays == null)
 		{
-			if(hashSet.isEmpty())
+			return null;
+		}
+		
+		Class<?> componentType = arrays.getClass().getComponentType().getComponentType();
+		
+		@SuppressWarnings("unchecked")
+		T[] emptyArray = (T[])Array.newInstance(componentType, 0);
+		
+		if(arrays.length == 0)
+		{
+			return emptyArray;
+		}
+		
+		for(T[] array : arrays)
+		{
+			if(array == null)
+			{
+				return emptyArray;
+			}
+		}
+		
+		Set<T> commonElements = new HashSet<>(List.of(arrays[0]));
+		
+		for(int i = 1; i < arrays.length; i++)
+		{
+			commonElements.retainAll(List.of(arrays[i]));
+			
+			if(commonElements.isEmpty())
 			{
 				break;
-			}
-			
-			if(hashSet.contains(element))
-			{
-				result.add(element);
-				hashSet.remove(element);
 			}
 		}
 		
 		@SuppressWarnings("unchecked")
-		E[] resultArray = (E[])Array.newInstance(array1.getClass().getComponentType(), result.size());
+		T[] resultArray = (T[])Array.newInstance(componentType, commonElements.size());
 		
-		return result.toArray(resultArray);
+		return commonElements.toArray(resultArray);
+	}
+	
+	/**
+	 * Checks if element is present in the array.
+	 * 
+	 * @param <T>
+	 * - type of the array's element.
+	 * @param array
+	 * - array to check.
+	 * @param element
+	 * - element to be found in the array.
+	 * @return
+	 * - <b>true</b> if the element was found.<br>
+	 * - <b>false</b> if the element wasn't found.
+	 */
+	public static <T> boolean contains(T[] array, T element)
+	{
+		for(int i = 0; i < array.length; i++)
+		{
+			if(Utilities.equals(array[i], element))
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Creates a {@link String} from the byte array. Each element is represented
+	 * as an hexadecimal number.
+	 * @param bytes
+	 * - bytes to be converted to the corresponding string.
+	 * @return
+	 * - {@link String} that represents the byte array with each element in hexadecimal.<br>
+	 * - <b>null</b> if the parameter is null.
+	 * @author Sergio Morel
+	 */
+	public static String toHexaArrayString(byte... bytes)
+	{
+		if(bytes == null)
+		{
+			return null;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("[");
+		
+		boolean includeSeparator = false;
+		
+		String separator = ", ";
+		
+		for(byte b : bytes)
+		{
+			if(includeSeparator)
+			{
+				sb.append(separator);
+			}
+			
+			sb.append(String.format("0x%02X", b));
+			
+			includeSeparator = true;
+		}
+		
+		sb.append("]");
+		
+		return sb.toString();
 	}
 }
---
+++ lib_utilidades/src/py/com/semp/lib/utilidades/utilities/Utilities.java
@@ -0,0 +1,148 @@
+package py.com.semp.lib.utilidades.utilities;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Class with static methods with utilities.
+ * 
+ * @author Sergio Morel
+ */
+public class Utilities
+{
+	/**
+	 * Checks if two objects are equal.
+	 * 
+	 * @param object1
+	 * - first object to check.
+	 * @param object2
+	 * - second object to check.
+	 * @return
+	 * - <b>true</b> if the objects are equal.<br>
+	 * - <b>false</b> if the objects are not equal.
+	 * @author Sergio Morel
+	 */
+	public static boolean equals(Object object1, Object object2)
+	{
+		if(object1 == object2)
+		{
+			return true;
+		}
+		
+		if(object1 == null || object2 == null)
+		{
+			return false;
+		}
+		
+		Class<?> type1 = object1.getClass();
+		Class<?> type2 = object2.getClass();
+		
+		if(type1.isArray() && type2.isArray())
+		{
+			if(!type1.equals(type2))
+			{
+				return false;
+			}
+			
+			if(type1.getComponentType().isPrimitive())
+			{
+				if(byte[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((byte[]) object1, (byte[]) object2);
+				}
+				else if(int[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((int[]) object1, (int[]) object2);
+				}
+				else if(short[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((short[]) object1, (short[]) object2);
+				}
+				else if(long[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((long[]) object1, (long[]) object2);
+				}
+				else if(float[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((float[]) object1, (float[]) object2);
+				}
+				else if(double[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((double[]) object1, (double[]) object2);
+				}
+				else if(char[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((char[]) object1, (char[]) object2);
+				}
+				else if(boolean[].class.isAssignableFrom(type1))
+				{
+					return Arrays.equals((boolean[]) object1, (boolean[]) object2);
+				}
+			}
+			
+			return Arrays.deepEquals((Object[])object1, (Object[])object2);
+		}
+		else if(Iterable.class.isAssignableFrom(type1) && Iterable.class.isAssignableFrom(type2))
+		{
+			return Utilities.equals((Iterable<?>) object1, (Iterable<?>) object2);
+		}
+		
+		return object1.equals(object2);
+	}
+	
+	/**
+	 * Compares if two iterables are equal.
+	 * @param iterable1
+	 * - first iterable
+	 * @param iterable2
+	 * - second iterable
+	 * @return
+	 * - <b>true</b> if the iterables are equal.<br>
+	 * - <b>false</b> if the iterables are not equal.
+	 * @author Sergio Morel
+	 */
+	public static boolean equals(Iterable<?> iterable1, Iterable<?> iterable2)
+	{
+		if(iterable1 == iterable2)
+		{
+			return true;
+		}
+		
+		if(iterable1 == null || iterable2 == null)
+		{
+			return false;
+		}
+		
+		Iterator<?> iterator1 = iterable1.iterator();
+		Iterator<?> iterator2 = iterable2.iterator();
+		
+		boolean hasNext1 = iterator1.hasNext();
+		boolean hasNext2 = iterator2.hasNext();
+		
+		if(hasNext1 != hasNext2)
+		{
+			return false;
+		}
+		
+		while(hasNext1)
+		{
+			Object element1 = iterator1.next();
+			Object element2 = iterator2.next();
+			
+			if(!Utilities.equals(element1, element2))
+			{
+				return false;
+			}
+			
+			hasNext1 = iterator1.hasNext();
+			hasNext2 = iterator2.hasNext();
+			
+			if(hasNext1 != hasNext2)
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
