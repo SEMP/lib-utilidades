487ac07d544397b182939bf2191b86c9a902ee87
Sergio Morel <sergio.morel@pti.org.py>
2023-08-21 16:00:33 -0400

    Test simple para CircularByteBuffer.

--- lib_utilidades/test/py/com/semp/lib/utilidades/data/CircularByteBufferTest.java
+++ lib_utilidades/test/py/com/semp/lib/utilidades/data/CircularByteBufferTest.java
@@ -1,8 +1,10 @@
 package py.com.semp.lib.utilidades.data;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +26,12 @@ class CircularByteBufferTest
 	private static final String[] JSON_FILE_NAME_EXTRACT_TEST =
 	{
 		"extract_test.json"
+	};
+	
+	private static final String[] JSON_FILE_NAME_CIRCULAR_BUFFER =
+	{
+		"circular_buffer_byteArray.json",
+		"circular_buffer_capacity.json"
 	};
 	
 	@ParameterizedTest
@@ -63,6 +71,48 @@ class CircularByteBufferTest
 		}
 	}
 	
+	@ParameterizedTest
+	@MethodSource("readCircularTestData")
+	public void testCircular(CircularTestDto testDTO)
+	{
+		String constructorType = testDTO.getConstructorType();
+		byte[] constructorArgument = testDTO.getConstructorArgument();
+		byte[] dataToAdd = testDTO.getDataToAdd();
+		int expectedSize = testDTO.getExpectedSize();
+		byte[] expectedData = testDTO.getExpectedData();
+		
+		CircularByteBuffer buffer = null;
+		
+		switch(constructorType)
+		{
+			case "capacity":
+			{
+				int capacity = constructorArgument[0];
+				
+				buffer = new CircularByteBuffer(capacity);
+				
+				break;
+			}
+			case "byteArray":
+			{
+				buffer = new CircularByteBuffer(constructorArgument);
+				
+				break;
+			}
+			
+			default: break;
+		}
+		
+		buffer.add(dataToAdd);
+		
+		byte[] obtainedData = buffer.getData();
+		
+		String message = testDTO.toString() + "Obtained: " + Arrays.toString(obtainedData);
+		
+		assertEquals(expectedSize, obtainedData.length);
+		assertArrayEquals(expectedData, obtainedData, message);
+	}
+	
 	private static Stream<Arguments> readExtractTestData() throws Exception
 	{
 		List<Arguments> testData = new LinkedList<>();
@@ -74,6 +124,26 @@ class CircularByteBufferTest
 			InputStream inputStream = CircularByteBufferTest.class.getResourceAsStream(DATA_DIRECTORY + fileName);
 			
 			List<ExtractTestDto> testDTOs = OBJECT_MAPPER.readValue(inputStream, typeReference);
+			
+			testDTOs.forEach(testDTO -> testDTO.setFileName(fileName));
+			
+			testData.addAll(testDTOs.stream().map(Arguments::of).collect(Collectors.toList()));
+		}
+		
+		return testData.stream();
+	}
+	
+	private static Stream<Arguments> readCircularTestData() throws Exception
+	{
+		List<Arguments> testData = new LinkedList<>();
+		
+		TypeReference<List<CircularTestDto>> typeReference = new TypeReference<List<CircularTestDto>>(){};
+		
+		for(String fileName : JSON_FILE_NAME_CIRCULAR_BUFFER)
+		{
+			InputStream inputStream = CircularByteBufferTest.class.getResourceAsStream(DATA_DIRECTORY + fileName);
+			
+			List<CircularTestDto> testDTOs = OBJECT_MAPPER.readValue(inputStream, typeReference);
 			
 			testDTOs.forEach(testDTO -> testDTO.setFileName(fileName));
 			
@@ -155,6 +225,87 @@ class CircularByteBufferTest
 			catch(JsonProcessingException e){}
             
 			return jsonString;
+		}
+	}
+	
+	private static class CircularTestDto
+	{
+		public String testScenario;
+		public String constructorType;
+		public byte[] constructorArgument;
+		public byte[] dataToAdd;
+		public int expectedSize;
+		public byte[] expectedData;
+		public String fileName;
+		
+		public String getTestScenario()
+		{
+			return testScenario;
+		}
+		
+		public String getConstructorType()
+		{
+			return constructorType;
+		}
+		
+		public byte[] getConstructorArgument()
+		{
+			return constructorArgument;
+		}
+		
+		public byte[] getDataToAdd()
+		{
+			return dataToAdd;
+		}
+		
+		public int getExpectedSize()
+		{
+			return expectedSize;
+		}
+		
+		public byte[] getExpectedData()
+		{
+			return expectedData;
+		}
+		
+		public String getFileName()
+		{
+			return this.fileName;
+		}
+		
+		public void setFileName(String fileName)
+		{
+			this.fileName = fileName;
+		}
+		
+		@Override
+		public String toString()
+		{
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("File: ").append(this.getFileName()).append(" | ");
+			sb.append("Name: ").append(this.getTestScenario()).append(" | ");
+			sb.append("Constructor Type: ").append(this.getConstructorType()).append(" | ");
+			sb.append("Constructor Argument: ").append(this.getString(this.getConstructorArgument())).append(" | ");
+			sb.append("Data to add: ").append(this.getString(this.getDataToAdd())).append(" | ");
+			sb.append("Expected size: ").append(this.getExpectedSize()).append(" | ");
+			sb.append("Expected data: ").append(this.getString(this.getExpectedData())).append(" | ");
+			
+			return sb.toString();
+		}
+		
+		private String getString(byte[] data)
+		{
+			return Arrays.toString(data);
+//            String jsonString = "[]";
+//            
+//			try
+//			{
+//				jsonString = OBJECT_MAPPER.writeValueAsString(data);
+//			}
+//			catch(JsonProcessingException e){}
+//            
+//			return jsonString;
 		}
 	}
 }
