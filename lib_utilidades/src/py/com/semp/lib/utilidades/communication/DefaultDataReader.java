ce0a44b62dc0c45651663cddbfc8d0ef4b656424
Sergio Morel <sergio.morel@pti.org.py>
2023-11-06 14:13:57 -0300

    Comentarios en DataReader. readingComplete() renombrado a isReadingComplete(). Comentarios en DefaultDataReader. Ajustes a metodo shutdown().

--- lib_utilidades/src/py/com/semp/lib/utilidades/communication/DefaultDataReader.java
+++ lib_utilidades/src/py/com/semp/lib/utilidades/communication/DefaultDataReader.java
@@ -24,6 +24,15 @@ import py.com.semp.lib.utilidades.log.Logger;
 import py.com.semp.lib.utilidades.log.LoggerManager;
 import py.com.semp.lib.utilidades.utilities.NamedThreadFactory;
 
+/**
+ * The {@link DefaultDataReader} class is responsible for asynchronously reading data from a data receiver
+ * that also acts as a data interface. It handles the life cycle of reading operations including start,
+ * stop, pause, and managing data notifications to listeners. This class also implements connection event
+ * listeners to react to connect/disconnect events.
+ *
+ * @param <T> The type of the data receiver that this reader will interact with, which must implement
+ *            both {@link DataReceiver} and {@link DataInterface}.
+ */
 public class DefaultDataReader<T extends DataReceiver & DataInterface> implements DataReader, ConnectionEventListener
 {
 	private static final String LISTENER_THREAD_NAME = "DefaultDataReaderListeners";
@@ -38,6 +47,11 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 	private static final Logger LOGGER = LoggerManager.getLogger(Values.Constants.UTILITIES_CONTEXT);
 	private final ExecutorService executorService = Executors.newFixedThreadPool(Values.Constants.LISTENERS_THREAD_POOL_SIZE, new NamedThreadFactory(LISTENER_THREAD_NAME));
 	
+	 /**
+     * Constructor for {@link DefaultDataReader}.
+     *
+     * @param dataReceiver The data receiver instance that will provide the data and configuration.
+     */
 	public DefaultDataReader(T dataReceiver)
 	{
 		super();
@@ -50,6 +64,9 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		this.stopping = false;
 	}
 	
+	 /**
+     * Main reading loop that runs asynchronously to fetch data from the receiver.
+     */
 	@Override
 	public void run()
 	{
@@ -123,26 +140,15 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		this.completeReading();
 	}
 	
-	private void shutdown()
-	{
-		try
-		{
-			this.dataReceiver.disconnect();
-			this.executorService.shutdownNow();
-		}
-		catch(CommunicationException e)
-		{
-			String errorMessage = MessageUtil.getMessage(Messages.SHUTDOWN_ERROR, this.getReceiverString(this.dataReceiver));
-			
-			LOGGER.error(errorMessage, e);
-		}
-		finally
-		{
-			this.isReading = false;
-			this.readingComplete = true;
-		}
-	}
-	
+	/**
+	 * Attempts to read data with a specified timeout. If data is not received within the timeout period, 
+	 * a CommunicationTimeoutException is thrown. This method repeatedly tries to read data until it is successful 
+	 * or the timeout is exceeded.
+	 *
+	 * @param readTimeoutMS the maximum time in milliseconds to wait for data to be read
+	 * @return the read data as a byte array
+	 * @throws CommunicationException if there is an issue with reading the data or a timeout occurs
+	 */
 	private byte[] readWithTimeout(Integer readTimeoutMS) throws CommunicationException
 	{
 		byte[] data;
@@ -168,6 +174,14 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		return data;
 	}
 	
+	/**
+	 * Retrieves a configuration value for a given name. If the value is not set, it returns the provided default value.
+	 * This method uses the configuration values from the data receiver to get the setting.
+	 *
+	 * @param name the name of the configuration to retrieve
+	 * @param defaultValue the default value to return if the configuration is not set
+	 * @return the value of the configuration, or the default value if not set
+	 */
 	private <C> C getConfiguration(String name, C defaultValue)
 	{
 		ConfigurationValues configurationValues = this.dataReceiver.getConfigurationValues();
@@ -181,11 +195,22 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		return value;
 	}
 	
+	/**
+	 * Gets the configuration values from the underlying data receiver.
+	 * 
+	 * @return
+	 * - Instance of {@link ConfigurationValues} from the underlying {@link DataReceiver}.
+	 */
 	private ConfigurationValues getConfigurationValues()
 	{
 		return this.dataReceiver.getConfigurationValues();
 	}
 	
+	/**
+	 * Completes the reading process. This method is called when reading is stopped either due to an interrupt or 
+	 * if the reader has finished its task. It closes the data receiver's connection, shuts down the executor service, 
+	 * and sets the flags indicating that reading is complete and the reader is no longer active.
+	 */
 	private void completeReading()
 	{
 		try
@@ -233,6 +258,11 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		}
 	}
 	
+	/**
+     * Informs all data listeners that new data has been received.
+     *
+     * @param data The data received from the data receiver.
+     */
 	private void informDataReceived(byte[] data)
 	{
 		Set<DataListener> dataListeners = this.dataReceiver.getDataListeners();
@@ -245,6 +275,14 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		});
 	}
 	
+	/**
+     * Submits a task to notify all listeners about an event.
+     *
+     * @param methodName The name of the method in which the listeners are notified.
+     * @param listeners The set of listeners to notify.
+     * @param notificationTask The consumer task that performs the notification.
+     * @param <L> The listener type.
+     */
 	private <L> void notifyListeners(String methodName, Set<L> listeners, Consumer<L> notificationTask)
 	{
 		try
@@ -279,6 +317,71 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 		}
 	}
 	
+	/**
+	 * Generates a string representation of the data receiver for logging purposes. This representation includes 
+	 * a timestamp and the identifier of the data receiver.
+	 *
+	 * @param dataInterface the data interface whose string representation is to be generated
+	 * @return a string representation of the data interface
+	 */
+	private String getReceiverString(DataInterface dataInterface)
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(Instant.now()).append(": ");
+		sb.append(dataInterface.getStringIdentifier());
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * This implementation immediately disconnects the data receiver and shuts down the executor service
+	 * without delay, marking the reading state as false and the reading process as complete, regardless
+	 * of the current reading state or any potential listeners.
+	 * </p>
+	 * @throws CommunicationException if any communication error occurs during the shutdown process.
+	 */
+	
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * This implementation immediately disconnects the data receiver and shuts down the executor service
+	 * swiftly, ensuring an immediate attempt to cease reading operations. It sets the reading state to false
+	 * and marks the reading process as complete. This is done irrespective of the current reading state or
+	 * ongoing tasks, and without ensuring that all listeners are informed of the shutdown.
+	 * </p>
+	 */
+	@Override
+	public void shutdown()
+	{
+		try
+		{
+			this.executorService.shutdownNow();
+			this.dataReceiver.shutdown();
+		}
+		catch(CommunicationException e)
+		{
+			String errorMessage = MessageUtil.getMessage(Messages.SHUTDOWN_ERROR, this.getReceiverString(this.dataReceiver));
+			
+			LOGGER.error(errorMessage, e);
+		}
+		finally
+		{
+			this.isReading = false;
+			this.readingComplete = true;
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * This implementation sets a flag to indicate that the reading process should be stopped.
+	 * The actual stopping of the process and resource cleanup should be handled elsewhere, likely
+	 * in the reading loop of the {@code run()} method.
+	 * </p>
+	 */
 	@Override
 	public void stopReading()
 	{
@@ -307,17 +410,7 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 	{
 		String errorMessage = MessageUtil.getMessage(Messages.CONNECTION_ERROR, this.getConfigurationValues().toString());
 		
-		LOGGER.error(errorMessage, exception);
-	}
-	
-	private String getReceiverString(DataInterface dataInterface)
-	{
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append(Instant.now()).append(": ");
-		sb.append(dataInterface.getStringIdentifier());
-		
-		return sb.toString();
+		LOGGER.warning(errorMessage, exception);
 	}
 	
 	@Override
@@ -325,7 +418,7 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 	{
 		String errorMessage = MessageUtil.getMessage(Messages.DISCONNECTION_ERROR, this.getReceiverString(dataInterface));
 		
-		LOGGER.error(errorMessage, exception);
+		LOGGER.warning(errorMessage, exception);
 	}
 	
 	@Override
@@ -341,7 +434,7 @@ public class DefaultDataReader<T extends DataReceiver & DataInterface> implement
 	}
 	
 	@Override
-	public boolean readingComplete()
+	public boolean isReadingComplete()
 	{
 		return this.readingComplete;
 	}
--- lib_utilidades/src/py/com/semp/lib/utilidades/communication/interfaces/DataReader.java
+++ lib_utilidades/src/py/com/semp/lib/utilidades/communication/interfaces/DataReader.java
@@ -1,10 +1,45 @@
 package py.com.semp.lib.utilidades.communication.interfaces;
 
+/**
+ * The {@link DataReader} interface defines the operations for reading data
+ * from a data source in a separate thread of execution.
+ */
 public interface DataReader extends Runnable
 {
+	 /**
+     * Initiates the reading process from a data source.
+     */
 	public void startReading();
+	
+	/**
+     * Temporarily pauses the reading process without stopping it entirely.
+     */
 	public void pauseReading();
+	
+	/**
+     * Permanently stops the reading process and releases any associated resources.
+     * After invoking this method, restarting the reading process may not be possible.
+     */
 	public void stopReading();
+	
+	/**
+     * Immediately shuts down the reader, attempting to stop operations as quickly as possible
+     * without concern for informing all listeners of the shutdown. This is in contrast to
+     * {@link #stopReading()}, which may perform a more graceful shutdown with notifications.
+     */
+    void shutdown();
+    
+    /**
+     * Checks if the reader is currently in the process of reading.
+     *
+     * @return {@code true} if the reader is currently reading, {@code false} otherwise.
+     */
 	public boolean isReading();
-	public boolean readingComplete();
+	
+	/**
+     * Checks if the reading process has been completed.
+     *
+     * @return {@code true} if the reading is complete, {@code false} otherwise.
+     */
+	public boolean isReadingComplete();
 }
