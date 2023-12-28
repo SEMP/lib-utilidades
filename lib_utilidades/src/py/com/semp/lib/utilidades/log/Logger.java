3217b84c614ecec22b1a4e5f24e7f51816ed3bde
Sergio Morel <sergio.morel@pti.org.py>
2023-12-28 13:33:08 -0300

    inclusión de info(Throwable throwable) y las correspondientes versiones para error, warning, debug y fatal en la interfaz Logger.

--- lib_utilidades/src/py/com/semp/lib/utilidades/log/Logger.java
+++ lib_utilidades/src/py/com/semp/lib/utilidades/log/Logger.java
@@ -54,6 +54,18 @@ public interface Logger
 	}
 	
 	/**
+	 * Logs a Throwable with the INFO level.
+	 *
+	 * @param throwable The throwable to log with INFO level.
+	 */
+	default void info(Throwable throwable)
+	{
+		String message = throwable.getLocalizedMessage();
+		
+		this.log(LogLevel.INFO, message != null ? message : throwable.toString(), throwable);
+	}
+	
+	/**
 	 * Logs an informational message.
 	 * 
 	 * @param message The message to log with INFO level.
@@ -76,27 +88,39 @@ public interface Logger
 	}
 	
 	/**
-     * Logs an informational message along with a Throwable.
-     * 
-     * @param message The message to log with INFO level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an informational message along with a Throwable.
+	 * 
+	 * @param message The message to log with INFO level.
+	 * @param throwable The throwable to log.
+	 */
 	default void info(String message, Throwable throwable)
 	{
 		this.log(LogLevel.INFO, message, throwable);
 	}
 	
 	/**
-     * Logs an object's string representation as an informational message along with a Throwable.
-     * 
-     * @param object The object whose string representation will be logged with INFO level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an object's string representation as an informational message along with a Throwable.
+	 * 
+	 * @param object The object whose string representation will be logged with INFO level.
+	 * @param throwable The throwable to log.
+	 */
 	default void info(Object object, Throwable throwable)
 	{
 		String message = Objects.toString(object);
 		
 		this.log(LogLevel.INFO, message, throwable);
+	}
+	
+	/**
+	 * Logs a Throwable with the ERROR level.
+	 *
+	 * @param throwable The throwable to log with ERROR level.
+	 */
+	default void error(Throwable throwable)
+	{
+		String message = throwable.getLocalizedMessage();
+		
+		this.log(LogLevel.ERROR, message != null ? message : throwable.toString(), throwable);
 	}
 	
 	/**
@@ -122,27 +146,39 @@ public interface Logger
 	}
 	
 	/**
-     * Logs an error message along with a Throwable.
-     * 
-     * @param message The message to log with ERROR level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an error message along with a Throwable.
+	 * 
+	 * @param message The message to log with ERROR level.
+	 * @param throwable The throwable to log.
+	 */
 	default void error(String message, Throwable throwable)
 	{
 		this.log(LogLevel.ERROR, message, throwable);
 	}
 	
 	/**
-     * Logs an object's string representation as an error message along with a Throwable.
-     * 
-     * @param object The object whose string representation will be logged with ERROR level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an object's string representation as an error message along with a Throwable.
+	 * 
+	 * @param object The object whose string representation will be logged with ERROR level.
+	 * @param throwable The throwable to log.
+	 */
 	default void error(Object object, Throwable throwable)
 	{
 		String message = Objects.toString(object);
 		
 		this.log(LogLevel.ERROR, message, throwable);
+	}
+	
+	/**
+	 * Logs a Throwable with the WARNING level.
+	 *
+	 * @param throwable The throwable to log with WARNING level.
+	 */
+	default void warning(Throwable throwable)
+	{
+		String message = throwable.getLocalizedMessage();
+		
+		this.log(LogLevel.WARNING, message != null ? message : throwable.toString(), throwable);
 	}
 	
 	/**
@@ -168,27 +204,39 @@ public interface Logger
 	}
 	
 	/**
-     * Logs a warning message along with a Throwable.
-     * 
-     * @param message The message to log with WARNING level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs a warning message along with a Throwable.
+	 * 
+	 * @param message The message to log with WARNING level.
+	 * @param throwable The throwable to log.
+	 */
 	default void warning(String message, Throwable throwable)
 	{
 		this.log(LogLevel.WARNING, message, throwable);
 	}
 	
 	/**
-     * Logs an object's string representation as a warning message along with a Throwable.
-     * 
-     * @param object The object whose string representation will be logged with WARNING level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an object's string representation as a warning message along with a Throwable.
+	 * 
+	 * @param object The object whose string representation will be logged with WARNING level.
+	 * @param throwable The throwable to log.
+	 */
 	default void warning(Object object, Throwable throwable)
 	{
 		String message = Objects.toString(object);
 		
 		this.log(LogLevel.WARNING, message, throwable);
+	}
+	
+	/**
+	 * Logs a Throwable with the DEBUG level.
+	 *
+	 * @param throwable The throwable to log with DEBUG level.
+	 */
+	default void debug(Throwable throwable)
+	{
+		String message = throwable.getLocalizedMessage();
+		
+		this.log(LogLevel.DEBUG, message != null ? message : throwable.toString(), throwable);
 	}
 	
 	/**
@@ -214,27 +262,39 @@ public interface Logger
 	}
 	
 	/**
-     * Logs a debug message along with a Throwable.
-     * 
-     * @param message The message to log with DEBUG level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs a debug message along with a Throwable.
+	 * 
+	 * @param message The message to log with DEBUG level.
+	 * @param throwable The throwable to log.
+	 */
 	default void debug(String message, Throwable throwable)
 	{
 		this.log(LogLevel.DEBUG, message, throwable);
 	}
 	
 	/**
-     * Logs an object's string representation as a debug message along with a Throwable.
-     * 
-     * @param object The object whose string representation will be logged with DEBUG level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an object's string representation as a debug message along with a Throwable.
+	 * 
+	 * @param object The object whose string representation will be logged with DEBUG level.
+	 * @param throwable The throwable to log.
+	 */
 	default void debug(Object object, Throwable throwable)
 	{
 		String message = Objects.toString(object);
 		
 		this.log(LogLevel.DEBUG, message, throwable);
+	}
+	
+	/**
+	 * Logs a Throwable with the FATAL level.
+	 *
+	 * @param throwable The throwable to log with FATAL level.
+	 */
+	default void fatal(Throwable throwable)
+	{
+		String message = throwable.getLocalizedMessage();
+		
+		this.log(LogLevel.FATAL, message != null ? message : throwable.toString(), throwable);
 	}
 	
 	/**
@@ -260,22 +320,22 @@ public interface Logger
 	}
 	
 	/**
-     * Logs a fatal error message along with a Throwable.
-     * 
-     * @param message The message to log with FATAL level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs a fatal error message along with a Throwable.
+	 * 
+	 * @param message The message to log with FATAL level.
+	 * @param throwable The throwable to log.
+	 */
 	default void fatal(String message, Throwable throwable)
 	{
 		this.log(LogLevel.FATAL, message, throwable);
 	}
 	
 	/**
-     * Logs an object's string representation as a fatal error message along with a Throwable.
-     * 
-     * @param object The object whose string representation will be logged with FATAL level.
-     * @param throwable The throwable to log.
-     */
+	 * Logs an object's string representation as a fatal error message along with a Throwable.
+	 * 
+	 * @param object The object whose string representation will be logged with FATAL level.
+	 * @param throwable The throwable to log.
+	 */
 	default void fatal(Object object, Throwable throwable)
 	{
 		String message = Objects.toString(object);
@@ -283,5 +343,12 @@ public interface Logger
 		this.log(LogLevel.FATAL, message, throwable);
 	}
 	
+	/**
+	 * Indicates if the logger is configured to log the DEBUG level messages.
+	 * 
+	 * @return
+	 * - <b>true</b> if DEBUG level messages are being logged.<br>
+	 * - <b>false</b> if DEBUG level messages are not being logged.
+	 */
 	public boolean isDebugging();
 }
